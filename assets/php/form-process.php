238db7cfd11649d2f2133ca48106a13ead1f3779
tobalo <nexxis817@gmail.com>
2021-08-12 11:46:30 -0500

    final tweaks for prod

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -1,7 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 $errorMSG = "";
 
